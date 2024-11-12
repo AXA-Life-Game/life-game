d8628d5f928008396c50e7c282da2df284a8e09f
pavophilip <pavophilip@gmail.com>
2024-11-12 13:34:13 +0100

    feat: prettier

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,29 +1,29 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Loader from "../components/Loader.jsx";
-import {Stack} from "@mui/system";
+import { Stack } from "@mui/system";
 import Button from "../components/Button.jsx";
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 1000)
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
 
-    return (
-        <Stack sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%'
-        }}>
-            {isLoading && <Loader/>}
-
-            <Button>Start</Button>
-        </Stack>
-    )
-}
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      {isLoading ? <Loader /> : <Button>Start</Button>}
+    </Stack>
+  );
+};
 
 export default Home;
