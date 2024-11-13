62a9446e6eae96fa244017491864983fa016219c
pavophilip <pavophilip@gmail.com>
2024-11-13 14:57:56 +0100

    recording

--- src/components/GameMenu.jsx
+++ src/components/GameMenu.jsx
@@ -1,18 +1,32 @@
-import { Stack } from "@mui/system";
+import { Box, Stack } from "@mui/system";
 import Logo from "./Logo.jsx";
 import { animated, config, useSpring } from "@react-spring/web";
 import Button from "./Button.jsx";
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import { useToggle } from "react-use";
+import { useInterval } from "usehooks-ts";
+import { differenceInSeconds, intervalToDuration } from "date-fns";
 
 const AnimatedButton = animated(Button);
+const RELEASE_DATE = new Date(2024, 11, 14, 15);
 
 const GameMenu = () => {
   const navigate = useNavigate();
 
   const ref = useRef(null);
   const [show, toggle] = useToggle(false);
+
+  useInterval(() => {
+    // Your custom logic here
+    console.log(
+      "count",
+      intervalToDuration({
+        start: 0,
+        end: differenceInSeconds(new Date(), RELEASE_DATE),
+      }),
+    );
+  }, 1000);
 
   const [buttonStyle] = useSpring(
     () => ({
@@ -35,14 +49,29 @@ const GameMenu = () => {
       }}
     >
       <Logo />
-      <AnimatedButton
-        style={buttonStyle}
-        onClick={() => {
-          navigate("/game/");
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        Start
-      </AnimatedButton>
+        <Box
+          sx={{
+            fontSize: 48,
+          }}
+        >
+          Coming Soon...
+        </Box>
+        <Box
+          sx={{
+            fontSize: 24,
+          }}
+        >
+          In 23 Hours
+        </Box>
+      </Box>
     </Stack>
   );
 };
