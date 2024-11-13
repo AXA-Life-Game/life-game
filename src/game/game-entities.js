c773b23d5e012211bf69c62e97edc98af1ab3c25
lucyk <lucykowalski10@gmail.com>
2024-11-13 10:03:12 +0100

    add life events to game entities

--- src/game/game-entities.js
+++ src/game/game-entities.js
@@ -1,4 +1,22 @@
 import axaLogo from "../assets/axa-logo.svg.png";
+import secondPillar from "../assets/events/second-pillar.png";
+import threeAInsurance from "../assets/events/three-a-insurance.png";
+import buyHouse from "../assets/events/buy-house.png";
+import divorce from "../assets/events/divorce.png";
+import education from "../assets/events/education.png";
+import flooding from "../assets/events/flooding.png";
+import getJob from "../assets/events/get-job.png";
+import houseInsurance from "../assets/events/house-insurance.png";
+import householdLiabilityInsurance from "../assets/events/household-liability-insurance.png";
+import jobLoss from "../assets/events/job-loss.png";
+import kids from "../assets/events/kids.png";
+import lottery from "../assets/events/lottery.png";
+import marriage from "../assets/events/marriage.png";
+import partTime from "../assets/events/part-time.png";
+import randomEvent from "../assets/events/random-event.png";
+import rentApartment from "../assets/events/rent-apartment.png";
+import robbery from "../assets/events/robbery.png";
+import salaryIncrease from "../assets/events/salary-increase.png";
 import larry from "../assets/larry-man-blue-walk.png";
 import babyLarry from "../assets/larry-man-blue-360.png";
 import background from "../assets/background.png";
@@ -7,6 +25,24 @@ export function loadEntities() {
   // load assets
   loadSprite("background", background);
   loadSprite("axaLogo", axaLogo);
+  loadSprite("secondPillar", secondPillar);
+  loadSprite("threeAInsurance", threeAInsurance);
+  loadSprite("buyHouse", buyHouse);
+  loadSprite("divorce", divorce);
+  loadSprite("education", education);
+  loadSprite("flooding", flooding);
+  loadSprite("getJob", getJob);
+  loadSprite("houseInsurance", houseInsurance);
+  loadSprite("householdLiabilityInsurance", householdLiabilityInsurance);
+  loadSprite("jobLoss", jobLoss);
+  loadSprite("kids", kids);
+  loadSprite("lottery", lottery);
+  loadSprite("marriage", marriage);
+  loadSprite("partTime", partTime);
+  loadSprite("randomEvent", randomEvent);
+  loadSprite("rentApartment", rentApartment);
+  loadSprite("robbery", robbery);
+  loadSprite("salaryIncrease", salaryIncrease);
   loadSprite("larry", larry, {
     sliceX: 4,
     sliceY: 0,
