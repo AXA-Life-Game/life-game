9765c06e9a4578ecec5f6457d9a2744f8645967b
Shanmathuran Sritharan <shanmathuran.sritharan@axa.ch>
2024-11-13 11:22:19 +0100

    add endgame and lostgame callbacks

--- src/engine/config.ts
+++ src/engine/config.ts
@@ -1,3 +1,5 @@
 export const GLOBAL_CONFIG = {
     startingAge: 18,
+    endGameAge: 70,
+    lostGameMoney: -15000,
   };
--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,53 +1,64 @@
-import { LifeEvent } from "./life-event";
-import { Lifebar } from "./lifebar";
-import { GameState, Month } from "./types";
+import {LifeEvent} from "./life-event";
+import {Lifebar} from "./lifebar";
+import {GameState, Month} from "./types";
+import {GLOBAL_CONFIG} from "./config";
 
 export class GameEngine {
     private state!: GameState;
     private lifeEventCollection: LifeEvent[] = [];
-  
-    constructor(lifeEvents: LifeEvent[], initialState?: GameState) {
+    private endGame: CallableFunction;
+    private lostGame: CallableFunction;
+
+    constructor(lifeEvents: LifeEvent[], endGame: CallableFunction, lostGame: CallableFunction, initialState?: GameState) {
         this.lifeEventCollection = lifeEvents;
-      if (initialState) {
-        this.state = initialState;
-      } else {
-        this.initLifebars();
-      }
+        this.endGame = endGame;
+        this.lostGame = lostGame;
+        if (initialState) {
+            this.state = initialState;
+        } else {
+            this.initLifebars();
+        }
     }
-  
+
     private initLifebars(): void {
-      this.state = {
-        lifebars: [
-          new Lifebar("MONEY", 10000),
-          new Lifebar("CHILDAGE", 0),
-          new Lifebar("INCOME", 0),
-          new Lifebar("TAX", 0),
-          new Lifebar("EXPENSES", 0),
-          new Lifebar("THIRDPILLAR", 0),
-          new Lifebar("SECONDPILLAR", 0),
-          new Lifebar("AGE", 18),
-        ],
-        collectedEvents: [],
-      };
+        this.state = {
+            lifebars: [
+                new Lifebar("MONEY", 10000),
+                new Lifebar("CHILDAGE", 0),
+                new Lifebar("INCOME", 0),
+                new Lifebar("TAX", 0),
+                new Lifebar("EXPENSES", 0),
+                new Lifebar("THIRDPILLAR", 0),
+                new Lifebar("SECONDPILLAR", 0),
+                new Lifebar("AGE", 18),
+            ],
+            collectedEvents: [],
+        };
     }
-  
+
     public addLifeEvent(lifeEvent: LifeEvent, time: Month): void {
-      lifeEvent.collectedTime = time;
-      this.lifeEventCollection.forEach((event) => {
-        event.mutate(lifeEvent);
-      });
-      this.state.lifebars.forEach((lifebar) => {
-        lifebar.applyEffect(lifeEvent.effect);
-      });
-      this.state.collectedEvents.push(lifeEvent);
+        lifeEvent.collectedTime = time;
+        this.lifeEventCollection.forEach((event) => {
+            event.mutate(lifeEvent);
+        });
+        this.state.lifebars.forEach((lifebar) => {
+            lifebar.applyEffect(lifeEvent.effect);
+        });
+        this.state.collectedEvents.push(lifeEvent);
     }
-  
+
     public progress(time: Month): GameEngine {
-      this.state.lifebars.forEach((lifebar) => {
-        lifebar.updateValueOnProgress(this.state.collectedEvents, time);
-      });
-      this.aggregateMoney(this.state.lifebars);
-      return this;
+        this.state.lifebars.forEach((lifebar) => {
+            lifebar.updateValueOnProgress(this.state.collectedEvents, time);
+        });
+        this.aggregateMoney(this.state.lifebars);
+        if (this.state.lifebars.find((lifebar) => lifebar.getType() === "MONEY")!.getValue() <= GLOBAL_CONFIG.lostGameMoney) {
+            this.lostGame();
+        }
+        if (this.state.lifebars.find((lifebar) => lifebar.getType() === "AGE")!.getValue() > GLOBAL_CONFIG.endGameAge) {
+            this.endGame();
+        }
+        return this;
     }
 
     private aggregateMoney(lifebars: Lifebar[]) {
@@ -59,28 +70,28 @@ export class GameEngine {
             money.setValue(money.getValue() + income.getValue() - expenses.getValue() - tax.getValue());
         }
     }
-  
+
     public getNextLifeEvent(): LifeEvent[] {
-      return this.lifeEventCollection
-        .filter(
-          (lifeEvent) => lifeEvent.frequency && lifeEvent.frequency.limit > 0
-        )
-        .filter(
-          (lifeEvent) =>
-            !lifeEvent.frequency!.probability ||
-            Math.random() < lifeEvent.frequency!.probability!
-        )
-        .map((lifeEvent) => {
-          lifeEvent.frequency!.limit -= 1;
-          return lifeEvent;
-        });
+        return this.lifeEventCollection
+            .filter(
+                (lifeEvent) => lifeEvent.frequency && lifeEvent.frequency.limit > 0
+            )
+            .filter(
+                (lifeEvent) =>
+                    !lifeEvent.frequency!.probability ||
+                    Math.random() < lifeEvent.frequency!.probability!
+            )
+            .map((lifeEvent) => {
+                lifeEvent.frequency!.limit -= 1;
+                return lifeEvent;
+            });
     }
-  
+
     public getLifeEventCollection(): LifeEvent[] {
-      return this.lifeEventCollection;
+        return this.lifeEventCollection;
     }
-  
+
     public getState(): GameState {
-      return this.state;
+        return this.state;
     }
-  }
+}
